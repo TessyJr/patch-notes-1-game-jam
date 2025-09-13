56bfa8a3589bbe4c497682b7480db9133264c373
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2025-09-13 14:05:10 +0700

    feat: player stays on top platform

--- Assets/PlatformController.cs
+++ Assets/PlatformController.cs
@@ -47,4 +47,20 @@ public class PlatformController : MonoBehaviour
         movingRight = goingRight;
         enabled = true;
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
 }
