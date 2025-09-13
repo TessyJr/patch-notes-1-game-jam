56d89e7df65f64df1811461db2dc1a14131214be
Dixon Willow <92355144+Dyxelta@users.noreply.github.com>
2025-09-13 22:32:16 +0700

    revamp: no material (#33)

--- Assets/Scripts/WorldMaterialChanger.cs
+++ Assets/Scripts/WorldMaterialChanger.cs
@@ -3,21 +3,35 @@ using UnityEngine.Tilemaps;
 
 public class WorldMaterialChanger : MonoBehaviour
 {
-    [SerializeField] private TilemapRenderer _tilemapRenderer;
     [SerializeField] private Tilemap _tilemap;
     [SerializeField] private SpriteRenderer _spriteRenderer;
-    [SerializeField] private Material _material;
+    [SerializeField] private TileBase _noMaterialTile;
     [SerializeField] private WorldSettingScriptableObject _worldSettingSO;
     void Start()
     {
         if (_worldSettingSO.MissingTexture)
         {
-            if (_tilemapRenderer != null) _tilemapRenderer.material = _material;
+            if (_spriteRenderer != null) _spriteRenderer.color = new(1, 0, 1, 1);
 
-            if (_spriteRenderer != null) _spriteRenderer.material = _material;
+            if (_tilemap != null)
+            {
+                _tilemap.color = new(1, 0, 1, 1);
 
-            if (_tilemap != null) _tilemap.color = new(1, 0, 1, 1);
+                if(_noMaterialTile && _worldSettingSO.GlitchyWorld == false) ReplaceAllTiles(_noMaterialTile);
+            }
         }
 
+    }
+
+    private void ReplaceAllTiles(TileBase newTile)
+    {
+        BoundsInt bounds = _tilemap.cellBounds;
+        foreach (var pos in bounds.allPositionsWithin)
+        {
+            if (_tilemap.HasTile(pos))
+            {
+                _tilemap.SetTile(pos, newTile);
+            }
+        }
     }
 }
