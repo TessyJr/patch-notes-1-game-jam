5d3effe09059d49b1c373e1dd5c73b73e8060161
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2025-09-14 23:13:20 +0700

    feat: error resetter

---
+++ Assets/Scripts/ErrorResetter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErrorResetter : MonoBehaviour
+{
+    [SerializeField] private PlayerSettingScriptableObject _playerSettingSO;
+    [SerializeField] private WorldSettingScriptableObject _worldSettingSO;
+
+    void Awake()
+    {
+        _playerSettingSO.Reset();
+        _worldSettingSO.Reset();
+    }
+}
