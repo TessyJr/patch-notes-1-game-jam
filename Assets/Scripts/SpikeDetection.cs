4a6bb10ca4d996d6d24f2cde28f8d73b57621103
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2025-09-14 14:21:10 +0700

    feat: death counter

--- Assets/Scripts/SpikeDetection.cs
+++ Assets/Scripts/SpikeDetection.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SpikeDetection : MonoBehaviour
 {
     [SerializeField] private SceneGameManager _sceneGameManager;
+    [SerializeField] private CounterScriptableObject _counterSO;
 
     public float restartDelay = 1f;
     [SerializeField] private GameObject _playerDeathPrefab;
@@ -11,17 +12,8 @@ public class SpikeDetection : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Trap"))
         {
-            SpawnDeathEffect();
-            Destroy(gameObject);
+            _counterSO.DeathCounter += 1;
 
-            _sceneGameManager.Restart(restartDelay);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Trap"))
-        {
             SpawnDeathEffect();
             Destroy(gameObject);
 
