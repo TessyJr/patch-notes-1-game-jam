6b5d1d0aa7102ca0b1b574ecc4d0160ce8eea7e4
vinchen <vinchen996@gmail.com>
2025-09-14 17:45:15 +0700

    Achievement Page

--- Assets/Scripts/AchievementManager.cs
+++ Assets/Scripts/AchievementManager.cs
@@ -6,10 +6,10 @@ using UnityEngine.UI;
 
 public class AchievementManager : MonoBehaviour
 {
-    [SerializeField] private List<AchievementItem> _achievements = new List<AchievementItem>();
     [SerializeField] private AchievementItem _activeAchievement;
     [SerializeField] private TextMeshProUGUI achievementText;
     [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private TextMeshProUGUI clueText;
     [SerializeField] private Image iconImage;
 
     public AchievementItem ActiveAchievement
@@ -27,11 +27,7 @@ public class AchievementManager : MonoBehaviour
 
     void Start()
     {
-        if (_achievements.Count > 0)
-        {
-            ActiveAchievement = _achievements[0];
-            OnAchievementChange(_activeAchievement);
-        }
+        OnAchievementChange(_activeAchievement);
     }
 
     private void OnAchievementChange(AchievementItem achievement)
@@ -39,6 +35,7 @@ public class AchievementManager : MonoBehaviour
         Achievement achievementSO = achievement.GetAchievementSO;
         achievement.SetIconImageColor(achievementSO.GetIsUnlocked ? Color.white : Color.black);
         achievementText.text = achievementSO.GetName;
+        clueText.text = achievementSO.GetClueText;
         iconImage.sprite = achievementSO.GetIcon;
         iconImage.color = achievementSO.GetIsUnlocked ? Color.white : Color.black;
         if (achievementSO.GetIsUnlocked)
