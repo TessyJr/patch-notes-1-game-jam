8fc2e1e93e429576602630b12eec79ef800818c3
Dixon Willow <92355144+Dyxelta@users.noreply.github.com>
2025-09-14 07:28:19 +0700

    fix: flag no texture still has texture (#41)

--- Assets/Scripts/WorldMaterialChanger.cs
+++ Assets/Scripts/WorldMaterialChanger.cs
@@ -6,12 +6,18 @@ public class WorldMaterialChanger : MonoBehaviour
     [SerializeField] private Tilemap _tilemap;
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private TileBase _noMaterialTile;
+    [SerializeField] private Sprite _sprite;
     [SerializeField] private WorldSettingScriptableObject _worldSettingSO;
     void Start()
     {
         if (_worldSettingSO.MissingTexture)
         {
-            if (_spriteRenderer != null) _spriteRenderer.color = new(1, 0, 1, 1);
+            if (_spriteRenderer != null)
+            {
+                _spriteRenderer.color = new(1, 0, 1, 1);
+
+                if (_sprite && _worldSettingSO.GlitchyWorld == false) _spriteRenderer.sprite = _sprite;
+            }
 
             if (_tilemap != null)
             {
